6d235c3e4d8ba93b971bc541f40e57817192b723
Dieter Ladenhauf <dieter.ladenhauf@dynatrace.com>
2020-10-14 07:58:23 +0200

    Introduce test checking for env var

--- cmd/mac-build-test/integration_test.go
+++ cmd/mac-build-test/integration_test.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"gotest.tools/assert"
@@ -12,4 +13,10 @@ import (
 func TestIntegrationAlwaysSucceeds(t *testing.T) {
 	fmt.Println("Integration test")
 	assert.Check(t, true, "Always succeeds")
+}
+
+func TestIntegrationWithEnvVar(t *testing.T) {
+	fmt.Println("Integration test with Env var")
+	envValue := os.Getenv("ENV_VAR")
+	assert.Equal(t, envValue, "42")
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,9 @@
 module github.com/didiladi/mac-build-test
 
-require gotest.tools v2.2.0+incompatible
+require (
+	github.com/google/go-cmp v0.5.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	gotest.tools v2.2.0+incompatible
+)
 
 go 1.13
